b87f8eebf30eca87a4e4fe54a953bd72b1fcea33
Abed El-Rahim Badr <abedbadr1999@hotmail.com>
2020-10-22 01:19:03 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { FilterComponent } from './filter/filter.component';
 
-const routes: Routes = [];
+import { HammerModule } from '@angular/platform-browser';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'filter', component: FilterComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HammerModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { FilterComponent } from './filter/filter.component';
+import { HomeComponent } from './home/home.component';
+import { CardSwipeComponent } from './card-swipe/card-swipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    FilterComponent,
+    HomeComponent,
+    CardSwipeComponent
   ],
   imports: [
     BrowserModule,
