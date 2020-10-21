de11709b4bd82e160fac022dc69ff209c39d670f
Abed El-Rahim Badr <abedbadr1999@hotmail.com>
2020-10-22 01:27:08 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { FilterComponent } from './filter/filter.component';
+import { CardSwipeComponent } from './card-swipe/card-swipe.component';
 
 import { HammerModule } from '@angular/platform-browser';
 
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'categories', component: CategoriesComponent },
   { path: 'filter', component: FilterComponent },
+  { path: 'card-swipe', component: CardSwipeComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
